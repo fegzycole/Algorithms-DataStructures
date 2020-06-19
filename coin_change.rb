6415aebfb35014e50f8fdd06fe81f84b39d7b8c9
ferguson <fergusoniyara@gmail.com>
2020-06-19 22:14:14 +0100

    Add Description

--- coin_change.rb
+++ coin_change.rb
@@ -1,3 +1,11 @@
+# You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+# Example 1:
+
+# Input: coins = [1, 2, 5], amount = 11
+# Output: 3 
+# Explanation: 11 = 5 + 5 + 1
+
 def coin_change(coins, amount)
   dp = []
   dp[0] = 0
