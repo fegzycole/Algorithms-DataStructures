126465342c98d453aa8e1151c20a2574031a612c
Ferguson Iyara <fergusoniyara@gmail.com>
2025-08-04 14:52:24 +0100

    feat: Add solution to product_of_array_except_self

---
+++ javascript/src/product_of_array_except_self/index.ts
@@ -0,0 +1,18 @@
+export const productExceptSelf = (nums: number[]): number[] => {
+  const n = nums.length;
+  const result = new Array(n).fill(1);
+
+  let prefix = 1;
+  for (let i = 0; i < n; i++) {
+    result[i] = prefix;
+    prefix *= nums[i];
+  }
+
+  let suffix = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    result[i] *= suffix;
+    suffix *= nums[i];
+  }
+
+  return result;
+};
