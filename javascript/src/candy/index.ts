ecc8a17c9baefcca4ab5d45b2ca4bfb64434b1e3
Ferguson Iyara <fergusoniyara@gmail.com>
2025-08-04 14:59:41 +0100

    feat: Add solution to candy

---
+++ javascript/src/candy/index.ts
@@ -0,0 +1,20 @@
+export const candy = (ratings: number[]): number => {
+  const n = ratings.length;
+  const candies = new Array(n).fill(1);
+
+  // Left to right pass
+  for (let i = 1; i < n; i++) {
+    if (ratings[i] > ratings[i - 1]) {
+      candies[i] = candies[i - 1] + 1;
+    }
+  }
+
+  // Right to left pass
+  for (let i = n - 2; i >= 0; i--) {
+    if (ratings[i] > ratings[i + 1]) {
+      candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+    }
+  }
+
+  return candies.reduce((sum, c) => sum + c, 0);
+};
