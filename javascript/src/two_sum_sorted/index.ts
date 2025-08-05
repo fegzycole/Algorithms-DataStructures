79d41f641e65d2c835bef2f79169238594a3b6fc
ferguson Iyara <fergusoniyara@gmail.com>
2025-08-05 14:01:33 +0100

    chore: Add solution to twoSum sorted

---
+++ javascript/src/two_sum_sorted/index.ts
@@ -0,0 +1,18 @@
+export const twoSum = (numbers: number[], target: number): number[] => {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    const sum = numbers[left] + numbers[right];
+
+    if (sum === target) {
+      return [left + 1, right + 1];
+    } else if (sum > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return [];
+};
